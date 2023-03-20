89b226bf685f314e390215e35126e22b5ba10191
Vinicius Pereira <119414265+ViniciusPereiraCardoso@users.noreply.github.com>
2023-03-19 22:51:44 -0300

    Carro

---
+++ Exercícios/Carro/src/main/java/Carro.java
@@ -0,0 +1,19 @@
+public class Carro {
+
+    String cor;
+    String marca;
+    double velocidadeMax;
+    double velocidadeAtual;
+
+    void ligar() {
+        System.out.println("Ligar");
+    }
+
+    void acelerar() {
+        System.out.println("Acelerar");
+    }
+
+    void mostraInfos(){
+        System.out.println("Mostra Infos");
+    }
+}
